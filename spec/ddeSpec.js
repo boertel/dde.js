21fa63e0e699071a8104f2ab788c17d6130002c3
boertel <ben@punchtab.com>
2012-11-20 19:07:11 -0800

    define few tests

--- spec/ddeSpec.js
+++ spec/ddeSpec.js
@@ -8,7 +8,15 @@ describe("Event Library", function () {
         expect(dde._env).toEqual(undefined);
         expect(dde._params).toEqual(undefined);
         
-        _settings = {a: 1, b: 2};
+        _default = {a: 1, b: 2};
+        _defaultObject = {a: {inside: true}, b: 2};
+
+        _simple = {c: 3, d: 4};
+        _simpleObject = {c: {insideC: "random"}, d: 4};
+
+        _overwrite = {a: "1bis", d: 4};
+        _overwriteObject = {a: {inside: false}, d: 4};
+
         callback = {
             one: function () {
                 //console.log("callback.one");
@@ -27,12 +35,143 @@ describe("Event Library", function () {
         spyOn(callback, 'two').andCallThrough();
     });
 
+    describe("unique host settings", function () {
 
-    it("set default settings", function () {
-        dde.default(_settings);
+        describe("default settings are empty", function () {
+            it("set empty default settings", function () {
+                dde.work({});
+            });
 
-        for (var key in _settings) {
-            expect(dde.get(key)).toEqual(_settings[key]);
-        }
+            it("set empty default settings and 1 environment", function () {
+                dde.work({});
+
+                dde.push(_simple);
+
+                for (var key in _simple) {
+                    expect(dde.get(key)).toEqual(_simple[key]);
+                }
+            });
+        });
+
+        describe("default settings are a simple object", function () {
+            it("set default settings", function () {
+                dde.work(_default);
+
+                for (var key in _default) {
+                    expect(dde.get(key)).toEqual(_default[key]);
+                }
+            });
+
+            it("1 environment", function () {
+                var key;
+
+                dde.work(_default);
+
+                dde.push(_simple);
+
+                for (key in _default) {
+                    expect(dde.get(key)).toEqual(_default[key]);
+                }
+
+                for (key in _simple) {
+                    expect(dde.get(key)).toEqual(_simple[key]);
+                }
+            });
+
+            it("1 environment overwriting 1 default setting", function () {
+                dde.work(_default);
+
+                dde.push(_overwrite);
+
+                expect(dde.get("a")).not.toEqual(_default["a"]);
+                expect(dde.get("a")).toEqual(_overwrite["a"]);
+            });
+        });
+
+        describe("default settings are a nested object", function () {
+            
+            it("1 environment", function () {
+                var key;
+
+                dde.work(_defaultObject);
+
+                dde.push(_simple);
+
+                for (key in _default) {
+                    expect(dde.get(key)).toEqual(_default[key]);
+                }
+
+                for (key in _simple) {
+                    expect(dde.get(key)).toEqual(_simple[key]);
+                }
+            });
+
+            it("get nested value", function () {
+                dde.work(_defaultObject);
+
+                expect(dde.get("a.inside")).toEqual(true);
+            });
+
+            it("get nested value that don't exist", function () {
+                dde.work(_defaultObject);
+
+                expect(dde.get("a.outsite")).toEqual(undefined);
+            });
+
+            it("1 environment with objects", function () {
+                var key;
+
+                dde.work(_defaultObject);
+
+                dde.push(_simpleObject);
+
+                for (key in _default) {
+                    expect(dde.get(key)).toEqual(_default[key]);
+                }
+
+                for (key in _simple) {
+                    expect(dde.get(key)).toEqual(_simple[key]);
+                }
+            });
+
+            it("1 environment with objects overwriting the default ones", function () {
+                var key;
+
+                dde.work(_defaultObject);
+
+                dde.push(_overwriteObject);
+
+                expect(dde.get("a")).not.toEqual(_defaultObject.a);
+                for (key in _simple) {
+                    expect(dde.get(key)).toEqual(_simple[key]);
+                }
+            });
+        });
+
+        describe("hash settings", function () {
+            beforeEach(function () {
+                _hash = "#dde_c=3&dde_d=4";
+                _hashOverwrite = "#dde_a=overwritten&dde_c=4";
+            });
+
+            it("basic", function () {
+                document.location.hash = _hash;
+
+                dde.work();
+
+                expect(dde.get("c")).toEqual(3);
+            });
+
+            it("overwrite settings", function () {
+                document.location.hash = _hashOverwrite;
+
+                dde.work(_default);
+
+                expect(dde.get("a")).not.toEqual(_default.a);
+                expect(dde.get("a")).toEqual("overwritten");
+            });
+        });
+
     });
+
 });
